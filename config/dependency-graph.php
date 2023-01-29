4657cda045996059b0e6c1402e4e0f4c01396c0d
Thavarajan.M <thavarajan@gmail.com>
2023-01-29 20:58:51 +0530

    added config

--- config/dependency-graph.php
+++ config/dependency-graph.php
@@ -2,5 +2,10 @@
 
 // config for MRTech/LaravelDependencyGraph
 return [
-
+    'display' => [
+        'show_methods' => false,
+        'show_properties' => false,
+        'show_data_type' => false,
+        'depth' => 2,
+    ],
 ];
