7825e2f80fde82d061411adfef510c39a26fc3ea
Thavarajan.M <thavarajan@gmail.com>
2023-01-29 19:11:59 +0530

    =Cofigured the name of the package

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace MRTech\LaravelDependencyGraph\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use MRTech\LaravelDependencyGraph\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
