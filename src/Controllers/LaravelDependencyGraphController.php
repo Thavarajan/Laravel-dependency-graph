d070948988c6fdb1b1fe03bbbff50ccc77c15382
Thavarajan.M <thavarajan@gmail.com>
2023-01-29 21:01:36 +0530

    controller added

---
+++ src/Controllers/LaravelDependencyGraphController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MRTech\LaravelDependencyGraph\Controllers;
+
+use Illuminate\Routing\Controller;
+use MRTech\LaravelDependencyGraph\Classes\DependencyChecker;
+
+class LaravelDependencyGraphController extends Controller
+{
+    /**
+     * Hold the service.
+     *
+     * @var DependencyChecker
+     */
+    public $dependencyChecker;
+
+    /**
+     * Constructor.
+     *
+     * @param DependencyChecker $service Reschedule service
+     */
+    public function __construct(DependencyChecker $service)
+    {
+        $this->dependencyChecker = $service;
+    }
+}
