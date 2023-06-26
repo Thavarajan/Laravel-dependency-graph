a38a2a30e65b8438ec61b65ce21ae597af552f65
Thavarajan.M <thavarajan@gmail.com>
2023-06-26 06:47:11 +0530

    sort the order

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,14 @@
 namespace MRTech\LaravelDependencyGraph\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use MRTech\LaravelDependencyGraph\LaravelDependencyGraphServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class TestCase extends Orchestra
 {
     protected function setUp(): void
@@ -17,13 +22,6 @@ class TestCase extends Orchestra
         );
     }
 
-    protected function getPackageProviders($app)
-    {
-        return [
-            LaravelDependencyGraphServiceProvider::class,
-        ];
-    }
-
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
@@ -32,5 +30,12 @@ class TestCase extends Orchestra
         $migration = include __DIR__.'/../database/migrations/create_laravel-dependency-graph_table.php.stub';
         $migration->up();
         */
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelDependencyGraphServiceProvider::class,
+        ];
     }
 }
